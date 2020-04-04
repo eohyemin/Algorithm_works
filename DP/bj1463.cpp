f780cc2ddef27d31187c8442d8c73dcb8acf373d
eohyemin <eohyemin@gmail.com>
2020-04-04 21:06:39 +0900

    baekjoon 1463 1로만들기

---
+++ DP/bj1463.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int MAX = 1000001;
+
+int dp[MAX];
+
+int f(int n) {
+
+	if (n == 1) return 0;
+	if (dp[n] != -1) return dp[n];
+
+	int result = f(n - 1) + 1;
+	if (n % 3 == 0) result = min(result, f(n / 3) + 1);
+	if (n % 2 == 0) result = min(result, f(n / 2) + 1);
+
+	dp[n] = result;
+	return result;
+
+}
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	fill(dp, dp+MAX, -1);
+	cout << f(n) << endl;
+
+	return 0;
+}
